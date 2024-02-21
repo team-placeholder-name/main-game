9858a2954aa378c3ab84fd7693a17870798f4484
Jared Goronkin <jgoronkin@gmail.com>
2024-02-21 12:06:28 -0500

    Hue Barriers

--- Pastel Production/Assets/_Scripts/Prismatic/States/Move.cs
+++ Pastel Production/Assets/_Scripts/Prismatic/States/Move.cs
@@ -17,32 +17,40 @@ namespace Prismatic
 
         public override void Update(SimulationData data)
         {
+            UpdatePosition(data);
+
+            UpdateView(data);
+        }
+
+        private void UpdatePosition(SimulationData data)
+        {
             float groundDistance = 0.6f;
-            Vector3 entityCenter = data.currentEntity.Position+ Vector3.up * 0.5f;
+            Vector3 entityCenter = data.currentEntity.Position + Vector3.up * 0.5f;
             Physics.Raycast(entityCenter, Vector3.down, out RaycastHit hit, groundDistance, 1 << LayerMask.NameToLayer("Default"));
             Vector3 movementNormal = hit.normal;
-            
 
-            
+
+
             Vector3 viewForward = (data.ViewTarget - data.ViewPosition);
             viewForward.y = 0;
             viewForward = viewForward.normalized;
             Vector3 moveForward = Vector3.ProjectOnPlane(viewForward, movementNormal).normalized;
-            Quaternion view = Quaternion.LookRotation(moveForward,Vector3.up);
+            Quaternion view = Quaternion.LookRotation(moveForward, Vector3.up);
             Matrix4x4 transformMatrix = Matrix4x4.Rotate(view);
 
-            Vector3 nextPosition = data.currentEntity.Position + (Vector3) (transformMatrix * new Vector3(movementInput.x, 0, movementInput.y) * Time.deltaTime * speed);
+            Vector3 nextPosition = data.currentEntity.Position + (Vector3)(transformMatrix * new Vector3(movementInput.x, 0, movementInput.y) * Time.deltaTime * speed);
 
 
             //Check for walls blocking movment
-            if(
-            Physics.SphereCast( entityCenter, 
+            if (
+            Physics.SphereCast(entityCenter,
                                 0.4f,
                                 nextPosition - data.currentEntity.Position,
-                                out hit, 
-                                (nextPosition - data.currentEntity.Position).magnitude, 
+                                out hit,
+                                (nextPosition - data.currentEntity.Position).magnitude,
                                 1 << LayerMask.NameToLayer("Default"))
-            ){
+            )
+            {
                 nextPosition = data.currentEntity.Position;
             }
 
@@ -51,17 +59,25 @@ namespace Prismatic
                 //snap the character to the ground
                 nextPosition.y = hit.point.y;
 
-            } 
+            }
             if (!Physics.Raycast(nextPosition + Vector3.up * 0.5f, Vector3.down, 1, 1 << LayerMask.NameToLayer("Default")))
             {
                 //check if the next position is a valid ground position
                 nextPosition = data.currentEntity.Position;
             }
-            Debug.DrawRay(data.currentEntity.Position, (nextPosition - data.currentEntity.Position).normalized, Color.red);
-            
-            data.currentEntity.Position = nextPosition;
 
-            UpdateView(data);
+            //Check if next position is within a HueBarrier
+            Collider[] hueBarriers = Physics.OverlapSphere(nextPosition, 0.5f, 1 << LayerMask.NameToLayer("HueBarrier"));
+            if(hueBarriers.Length>0)
+            {
+                if (hueBarriers[0].gameObject.GetComponent<HueBarrier>().hue != data.currentEntity.HueMix)
+                {
+                    nextPosition = data.currentEntity.Position;
+                }
+            }
+            
+            Debug.DrawRay(data.currentEntity.Position, (nextPosition - data.currentEntity.Position).normalized, Color.red);
+            data.currentEntity.Position = nextPosition;
         }
 
         public override void OnMoveInput(Vector2 movementInput)
