400a45979d495e11d6bbf344e0ebe41638720aee
Jared Goronkin <jgoronkin@gmail.com>
2024-02-21 20:00:40 -0500

    Level 4 complete & Minor Bug Fixes

--- Pastel Production/Assets/_Scripts/Prismatic/States/Project.cs
+++ Pastel Production/Assets/_Scripts/Prismatic/States/Project.cs
@@ -49,7 +49,9 @@ namespace Prismatic
 
                 //First check for obstructions
                 Vector3 entityOffset = entityToCheck.Position -data.currentEntity.Position;
-                if (Physics.Raycast(data.currentEntity.Position+Vector3.up*0.5f, entityOffset, entityOffset.magnitude, 1 << LayerMask.NameToLayer("Default")))
+                if (Physics.Raycast(data.currentEntity.Position+Vector3.up*0.5f, entityOffset, entityOffset.magnitude, 
+                    (1 << LayerMask.NameToLayer("Default"))+ 
+                    (1 << LayerMask.NameToLayer("HueBarrier"))))
                 {
                     Debug.Log("Entity Blocked");
                     continue;
