658bc16905c948e67986d73123043e26ec449c22
amonjerro <eescudero_arens@hotmail.com>
2024-03-06 20:47:29 -0500

    Simple onboarding system

---
+++ Pastel Production/Assets/_Scripts/Onboarding/OnboardingTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnboardingTrigger : MonoBehaviour
+{
+    public OnboardingManager manager;
+    private bool _ReadyToTrigger;
+
+    private void Awake()
+    {
+        _ReadyToTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_ReadyToTrigger && other.gameObject.CompareTag("Player"))
+        {
+            manager.ShowNextMessage();
+            _ReadyToTrigger = false;
+        }
+    }
+}
